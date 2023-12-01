2fcabffab32f2c7468f778f0a40a04303f8b4d38
Jim Turner <jimturner@appdevforall.com>
2023-12-01 08:44:12 -0800

    updated SDK to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.appdevforall.gittest"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.appdevforall.gittest"
-        minSdk = 33
-        targetSdk = 33
+        minSdk = 34
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
